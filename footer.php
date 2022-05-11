af12d02868a08a866977e5918bff7027b5784c24
Anika-nawer <anikanawer8@gmail.com>
2022-05-11 16:43:47 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+<section class="footer">
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="#">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +8801713448771</p>
+         <p> <i class="fas fa-phone"></i> +8801556132287</p>
+         <p> <i class="fas fa-envelope"></i>bookmart@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Dinajpur,Bangladesh </p>
+      </div>
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+   </div>
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Book Mart</span> </p>
+
+</section>
